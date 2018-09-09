5e8148ae0595c3b60e502699d56a5f7537dfa804
Jerico Esguerra Micutuan <jmicutuan@jericos-macbook-pro.local>
2018-09-09 21:22:55 +0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Items.swift
+//  Todoey
+//
+//  Created by Jerico Esguerra Micutuan on 09/09/2018.
+//  Copyright © 2018 Captd.LazyBear. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done : Bool = false
+    
+}
